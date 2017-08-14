90ce2925102311f97702ca97162cc7ebec71f56c
Victor Wads <victorcleber@hotmail.com>
2017-08-14 03:10:33 -0300

    Big Update
    Projects and Skills

--- js/https.js
+++ js/https.js
@@ -1,5 +1,5 @@
-if(window.location.protocol == 'http:')
-	window.location.protocol = 'https:';
+// if(window.location.protocol == 'http:')
+// 	window.location.protocol = 'https:';
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
