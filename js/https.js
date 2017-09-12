ed7aa244d617e6720067a8b9e95e12ebcdff48ee
Victor Wads <victorcleber@hotmail.com>
2017-09-12 00:14:37 -0300

    Private Analytics

--- js/https.js
+++ js/https.js
@@ -10,24 +10,5 @@
 ga('create', 'UA-104206956-1', 'auto');
 ga('send', 'pageview');
 
-//private Analytics
-(function (){
-	var serverHost = "https://server.privateanalytics.kavi.ga/";
-	function s4() {return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);}
-	var SUID = s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
-	var type = "SendAcesso";
-	var token;
-	function request(init){
-		json = {
-			SUID: SUID,
-			URL: window.location.href,
-			type: init===undefined ? 1 : 0
-		};
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open("POST", serverHost);
-		xmlhttp.setRequestHeader("Content-Type", "application/json");
-		xmlhttp.send(token+'\n'+type+'\n'+JSON.stringify(json));
-	}
-	request(true);
-	setInterval(request, 5000);
-})();
+//Private Analytics
+(function(i,s,o,g,a,m){a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://privateanalytics.kavi.ga/embeded/script.js');
